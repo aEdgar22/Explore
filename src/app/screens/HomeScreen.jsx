73b62730efd530ce8069a1da06ab98d3eedd1430
edgarElite <edgareliteapartments@gmail.com>
2024-04-26 14:18:05 -0500

    Added HomeScreen

---
+++ src/app/screens/HomeScreen.jsx
@@ -0,0 +1,17 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import useAuthStore from '../../store/authStore';
+import LogoutButton from '../auth/components/LogoutButton';
+
+const HomeScreen = () => {
+ 
+
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Bienvenido a la App</Text>
+      <LogoutButton  />
+    </View>
+  )
+}
+
+export default HomeScreen
